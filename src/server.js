a8225984b158393621585fc16fd25e4566df5cdd
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-23 12:53:55 -0300

    create route GET and POST tasks

--- src/server.js
+++ src/server.js
@@ -1,6 +1,46 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
 
-const server = http.createServer((request, response) => {
+const tasks = [];
+
+const server = http.createServer(async (request, response) => {
+  const buffers = [];
+
+  for await (const chunk of buffers) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
+
+  response.setHeader("Content-Type", "application/json");
+
+  if (request.method === "GET" && request.url === "/tasks") {
+    return response.writeHead(200).end(JSON.stringify(tasks));
+  }
+
+  if (request.method === "POST" && request.url === "/tasks") {
+    const { title, description } = request.body;
+
+    if (title || description) {
+      throw new Error("Preencha todos os campos para criar uma nova tarefa");
+    }
+
+    tasks.push({
+      id: randomUUID(),
+      title,
+      description,
+      completed_at: null,
+      created_at: Date.now(),
+      updated_at: Date.now(),
+    });
+
+    return response.writeHead(201).end();
+  }
+
   return response.end("Hello World");
 });
 
